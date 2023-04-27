9027e8c5d22f8d64b320b07419ec7f76ae090aac
Venosa257 <102314311+Venosa257@users.noreply.github.com>
2023-04-27 23:59:57 +0700

    Delete smeas directory

--- smeas/cek-login.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-
-    if(!isset($_SESSION['username'])){ //session belum didaftarkan
-        header("Location: index.php"); //redirect ke halaman login
-    }
- ?>
--- smeas/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    session_start();
-
-    unset($_SESSION['username']); //unset session
-    header("Location: index.php"); //redirect ke halaman login
-
-
- ?>
